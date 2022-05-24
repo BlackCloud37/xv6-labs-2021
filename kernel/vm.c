c80b67493464d8c2c4df53e3a4e0bfac9fcd781f
chen.ye <chen.ye@realai.ai>
2022-05-24 22:21:05 +0800

    print a pagetable

--- kernel/vm.c
+++ kernel/vm.c
@@ -432,3 +432,29 @@ copyinstr(pagetable_t pagetable, char *dst, uint64 srcva, uint64 max)
     return -1;
   }
 }
+
+void pagetable_print(pagetable_t pagetable, int depth) {
+  if (depth == 1) {
+    printf("page table %p\n", pagetable);
+  }
+  if (depth > 3) {
+    panic("vmprint: unreachable code");
+  }
+  for (int i = 0; i < 512; i++) {
+    pte_t pte = pagetable[i];
+    if((pte & PTE_V)) {
+      for (int j = 0; j < depth; j++) {
+        printf(" ..");
+      }
+      printf("%d: pte %p pa %p\n", i, pte, PTE2PA(pte));
+    }
+    if((pte & PTE_V) && (pte & (PTE_R|PTE_W|PTE_X)) == 0) {
+      // this pte points to next level pagetable
+      pagetable_print((pagetable_t)PTE2PA(pte), depth + 1); 
+    }
+  }
+}
+
+void vmprint(pagetable_t pagetable) {
+  pagetable_print(pagetable, 1);
+}
