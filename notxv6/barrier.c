412b5a6e5e850858af0cee8e7f4cbc1b4a31d648
chen.ye <chen.ye@realai.ai>
2022-05-29 00:15:01 +0800

    barrier and time

--- notxv6/barrier.c
+++ notxv6/barrier.c
@@ -30,7 +30,21 @@ barrier()
   // Block until all threads have called barrier() and
   // then increment bstate.round.
   //
-  
+  pthread_mutex_lock(&bstate.barrier_mutex);
+  bstate.nthread += 1;
+  if (bstate.nthread == nthread)
+  {
+    // last thread
+    bstate.round += 1;
+    bstate.nthread = 0;
+    pthread_mutex_unlock(&bstate.barrier_mutex);
+    pthread_cond_broadcast(&bstate.barrier_cond);
+  }
+  else
+  {
+    pthread_cond_wait(&bstate.barrier_cond, &bstate.barrier_mutex);
+    pthread_mutex_unlock(&bstate.barrier_mutex);
+  }
 }
 
 static void *
