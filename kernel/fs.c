a4a7bad69745741b91d387267cce386548c0434d
chen.ye <chen.ye@realai.ai>
2022-05-29 21:07:30 +0800

    bigfile

--- kernel/fs.c
+++ kernel/fs.c
@@ -401,6 +401,38 @@ bmap(struct inode *ip, uint bn)
     return addr;
   }
 
+  bn -= NINDIRECT;
+  if (bn < NINDIRECT * NINDIRECT)
+  {
+    // first level
+    if ((addr = ip->addrs[NDIRECT + 1]) == 0)
+    {
+      ip->addrs[NDIRECT + 1] = addr = balloc(ip->dev);
+    }
+    bp = bread(ip->dev, addr);
+    a = (uint *)bp->data;
+
+    // second level
+    if ((addr = a[bn / NINDIRECT]) == 0)
+    {
+      a[bn / NINDIRECT] = addr = balloc(ip->dev);
+      log_write(bp);
+    }
+    brelse(bp);
+
+    bp = bread(ip->dev, addr);
+    a = (uint *)bp->data;
+
+    // block
+    if ((addr = a[bn % NINDIRECT]) == 0)
+    {
+      a[bn % NINDIRECT] = addr = balloc(ip->dev);
+      log_write(bp);
+    }
+    brelse(bp);
+    return addr;
+  }
+
   panic("bmap: out of range");
 }
 
@@ -430,6 +462,31 @@ itrunc(struct inode *ip)
     brelse(bp);
     bfree(ip->dev, ip->addrs[NDIRECT]);
     ip->addrs[NDIRECT] = 0;
+  }
+
+  if (ip->addrs[NDIRECT + 1])
+  {
+    bp = bread(ip->dev, ip->addrs[NDIRECT + 1]);
+    a = (uint *)bp->data;
+
+    for (j = 0; j < NINDIRECT; j++)
+    {
+      if (a[j])
+      {
+        struct buf *bpd = bread(ip->dev, a[j]);
+        uint *b = (uint *)bpd->data;
+        for (int k = 0; k < NINDIRECT; k++)
+        {
+          if (b[k])
+            bfree(ip->dev, b[k]);
+        }
+        brelse(bpd);
+        bfree(ip->dev, a[j]);
+      }
+    }
+    brelse(bp);
+    bfree(ip->dev, ip->addrs[NDIRECT + 1]);
+    ip->addrs[NDIRECT + 1] = 0;
   }
 
   ip->size = 0;
